d8c01788e280e04a4117f6b542e453d2970e2ac1
Sedat Yusuf Ergüneş <s.y.ergunes@gmail.com>
2014-10-10 15:19:41 +0300

    Authorization error fixed

--- gmail.sh
+++ gmail.sh
@@ -2,13 +2,13 @@
 
 #Config
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-username="YOUR_GMAIL_USERNAME"
-password="YOUR_GMAIL_PASSWORD"
+username="USERNAME_HERE"
+password="PASSWORD_HERE"
 
 #Authorization Check
-auth=$(curl -su $username:$password https://mail.google.com/mail/feed/atom | grep "Unauthorized")
+auth=$(curl -su $username:$password https://mail.google.com/mail/feed/atom | grep -ci "Unauthorized")
 
-if [ "$auth" != 0 ]; then
+if [ "$auth" != "0" ]; then
     notify-send -i $DIR/icons/gmail-notification-icon.png "Gmail" "Authorization error! Check your account settings...";
 else
     mail_count=$(curl -su $username:$password https://mail.google.com/mail/feed/atom | grep "<fullcount>" | cut -d'>' -f8 | cut -d'<' -f1
@@ -18,4 +18,3 @@ else
         notify-send -i $DIR/icons/gmail-notification-icon.png "Gmail" "You got $mail_count new email!";
     fi
 fi
-
