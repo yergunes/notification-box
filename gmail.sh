56946545a2940d198ef0291f84b53f6874ed5ced
Sedat Yusuf Ergüneş <s.y.ergunes@gmail.com>
2014-10-10 14:45:37 +0300

    Authorization check fixed

--- gmail.sh
+++ gmail.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-#CONFIG
+#Config
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 username="YOUR_GMAIL_USERNAME"
 password="YOUR_GMAIL_PASSWORD"
 
-mail_count=$(curl -su $username:$password https://mail.google.com/mail/feed/atom | grep "<fullcount>" | cut -d'>' -f8 | cut -d'<' -f1
-)
+#Authorization Check
+auth=$(curl -su $username:$password https://mail.google.com/mail/feed/atom | grep "Unauthorized")
 
-if [ "$mail_count" != "0" ]; then
-	notify-send -i $DIR/icons/gmail-notification-icon.png "Gmail" "You got $mail_count new email!";
+if [ "$auth" != 0 ]; then
+    notify-send -i $DIR/icons/gmail-notification-icon.png "Gmail" "Authorization error! Check your account settings...";
+else
+    mail_count=$(curl -su $username:$password https://mail.google.com/mail/feed/atom | grep "<fullcount>" | cut -d'>' -f8 | cut -d'<' -f1
+    )
+
+    if [ "$mail_count" != "0" ]; then
+        notify-send -i $DIR/icons/gmail-notification-icon.png "Gmail" "You got $mail_count new email!";
+    fi
 fi
 
-#Yetki kontrolü yap Unauthorized'e göre
