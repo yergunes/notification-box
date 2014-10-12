e6e4a43af7f64c6ff265409567ee7309172ef8d8
Sedat Yusuf Ergüneş <s.y.ergunes@gmail.com>
2014-10-12 20:52:32 +0300

    GUI added.

---
+++ moduls/gmail.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Gathering Account Informations
+informations=$(cat $BASE/moduls/.gmail-config);
+I=$IFS; IFS="" 
+for STATEMENTS in $informations; do 
+   eval $STATEMENTS 
+done 
+IFS=$I 
+
+#Authorization Check
+auth=$(curl -su $gmail_username:$gmail_password https://mail.google.com/mail/feed/atom | grep -ci "Unauthorized")
+
+if [ "$auth" != "0" ]; then
+    notify-send -i $BASE/icons/gmail-notification-icon.png "Gmail" "Authorization error! Check your account settings...";
+else
+    while [ 1 ]; do
+         mail_count=$(curl -su $gmail_username:$gmail_password https://mail.google.com/mail/feed/atom | grep "<fullcount>" | cut -d'>' -f8 | cut -d'<' -f1
+        )
+
+        if [ "$mail_count" != "0" ]; then
+            notify-send -i $BASE/icons/gmail-notification-icon.png "Gmail" "You got $mail_count new email!";
+        fi
+    sleep $gmail_refresh_time;
+    done
+fi
