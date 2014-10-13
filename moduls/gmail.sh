6de2b9b7476fb7f4a4ab4d4be927ed51a4702e75
Sedat Yusuf Ergüneş <s.y.ergunes@gmail.com>
2014-10-13 21:20:26 +0300

    Icon path fixed.

--- moduls/gmail.sh
+++ moduls/gmail.sh
@@ -12,14 +12,14 @@ IFS=$I
 auth=$(curl -su $gmail_username:$gmail_password https://mail.google.com/mail/feed/atom | grep -ci "Unauthorized")
 
 if [ "$auth" != "0" ]; then
-    notify-send -i $BASE/icons/gmail-notification-icon.png "Gmail" "Authorization error! Check your account settings...";
+    notify-send -i $BASE/icons/gmail-icon.png "Gmail" "Authorization error! Check your account settings...";
 else
     while [ 1 ]; do
          mail_count=$(curl -su $gmail_username:$gmail_password https://mail.google.com/mail/feed/atom | grep "<fullcount>" | cut -d'>' -f8 | cut -d'<' -f1
         )
 
         if [ "$mail_count" != "0" ]; then
-            notify-send -i $BASE/icons/gmail-notification-icon.png "Gmail" "You got $mail_count new email!";
+            notify-send -i $BASE/icons/gmail-icon.png "Gmail" "You got $mail_count new email!";
         fi
     sleep $gmail_refresh_time;
     done
