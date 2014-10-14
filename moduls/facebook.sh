5abd5e71274921728afd651bea0823f4ad18e0ee
Sedat Yusuf Ergüneş <s.y.ergunes@gmail.com>
2014-10-14 21:31:24 +0300

    Facebook module added.

---
+++ moduls/facebook.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+informations=$(cat $BASE/moduls/.facebook-config);
+I=$IFS; IFS="" 
+for STATEMENTS in $informations; do 
+   eval $STATEMENTS 
+done 
+IFS=$I
+ 
+COOKIES='/tmp/cookies.txt'
+USER_AGENT='Mozilla/5.0'
+ 
+
+while [ 1 ]; do
+
+    curl -s -X GET 'https://www.facebook.com/home.php' --verbose --user-agent $USER_AGENT --cookie $COOKIES --cookie-jar $COOKIES --location # redirects to https://login.facebook.com/login.php
+    curl -s -X POST 'https://login.facebook.com/login.php' --verbose --user-agent $USER_AGENT --data-urlencode "email=$facebook_username" --data-urlencode "pass=$facebook_password" --cookie $COOKIES --cookie-jar $COOKIES
+    curl -s -X GET 'https://www.facebook.com/home.php' --verbose --user-agent $USER_AGENT --cookie $COOKIES --cookie-jar $COOKIES > /tmp/facebook;
+
+
+    if [ "$CHECKBOX_FACEBOOK_MESSAGE" = "true" ]; then
+        message_count=$(cat /tmp/facebook | grep -o -m 1 ".\{0,0\}mercurymessagesCountValue.\{0,10\}" | cut -d">" -f2 | cut -d"<" -f1);
+    fi
+
+    if [ "$CHECKBOX_FACEBOOK_NOTIFICATION" = "true" ]; then
+        notification_count=$(cat /tmp/facebook | grep -o -m 1 ".\{0,0\}notificationsCountValue.\{0,10\}" | cut -d">" -f2 | cut -d"<" -f1);
+    fi
+
+
+    if [ "$message_count" != "0" ] && [ "$notification_count" != "0" ]; then
+        notify-send -i $BASE/../icons/facebook-icon.png "facebook" "You got $message_count new notification and $notification_count new message"
+    elif [ "$message_count" != "0" ] && [ "$message_count" != "" ]; then
+        notify-send -i $BASE/../icons/facebook-icon.png "facebook" "You got $message_count new message"
+    elif [ "$notification_count" != "0" ] && [ "$notification_count" != "" ]; then
+        notify-send -i $BASE/../icons/facebook-icon.png "facebook" "You got $notification_count new notification"
+    fi
+
+sleep $facebook_refresh_time;
+done
--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #COPY PROGRAM IN BIN FOLDER
 [[ ! -f /bin/notification/box ]] && sudo cp $BASE/notification-box /bin
 #GENERAL APPLICATION SETTINGS
-echo -e -n 'CHECKBOX_GMAIL="false" CHECKBOX_WORDPRESS="false" CHECKBOX_OPENGUI="false" CHECKBOX_STARTUP="false" CHECKBOX_UPDATE="false"' > $HOME/.notification-box;
+echo -e -n 'CHECKBOX_GMAIL="false" CHECKBOX_FACEBOOK="false" CHECKBOX_WORDPRESS="false" CHECKBOX_HIDEGUI="false" CHECKBOX_STARTUP="false"' > $HOME/.notification-box;
 #GMAIL CONFIG FILE PLACEHOLDER
 echo 'gmail_username="USERNAME" gmail_password="PASSWORD" gmail_refresh_time="3600"' > $BASE/moduls/.gmail-config;
+#FACEBOOK CONFIG FILE PLACEHOLDER
+echo 'facebook_username="USERNAME" facebook_password="PASSWORD" facebook_refresh_time="3600"' CHECKBOX_FACEBOOK_MESSAGE="false" CHECKBOX_FACEBOOK_NOTIFICATION="false" > $BASE/moduls/.facebook-config;
 #WORDPRESS CONFIG FILE PLACEHOLDER
 echo 'wordpress_url="URL" wordpress_username="ADMIN" wordpress_password="PASSWORD" wordpress_refresh_time="3600" CHECKBOX_WORDPRESS_UPDATE="false" CHECKBOX_WORDPRESS_COMMENT="false"' > $BASE/moduls/.wordpress-config;
